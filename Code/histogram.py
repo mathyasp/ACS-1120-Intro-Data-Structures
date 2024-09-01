8ab7dc2069ab320de89352d22069dd3e08f4d882
mathyasp <mathyastp@gmail.com>
2024-09-01 13:09:24 -0400

    Added unique_words function

--- Code/histogram.py
+++ Code/histogram.py
@@ -11,6 +11,20 @@ def histogram(source_text):
 			histogram[word] = 1
 	return histogram
 
+def unique_words(histogram):
+	unique_words = []
+	for key in histogram:
+		if histogram[key] == 1:
+			unique_words.append(key)
+	return unique_words
+
 if __name__ == '__main__':
 	source_text = sys.argv[1]
-	print(histogram(source_text))
+	histogram = histogram(source_text)
+	print('~-~-~-~-~-~-~-~-~ Histogram ~-~-~-~-~-~-~-~-~')
+	print(histogram)
+	unique = sys.argv[2:]
+	if 'unique_words' in unique:
+		print('\n~-~-~-~-~-~-~-~-~ Unique Words ~-~-~-~-~-~-~-~-~')
+		print(unique_words(histogram))
+		
