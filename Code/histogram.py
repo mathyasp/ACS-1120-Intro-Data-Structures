6db2fa0e8fd691aedf5d97506f1268807d0e5615
mathyasp <mathyastp@gmail.com>
2024-09-01 13:13:48 -0400

    Added frequency function

--- Code/histogram.py
+++ Code/histogram.py
@@ -18,13 +18,20 @@ def unique_words(histogram):
 			unique_words.append(key)
 	return unique_words
 
+def frequency(word, histogram):
+	return histogram[word]
+
 if __name__ == '__main__':
-	source_text = sys.argv[1]
-	histogram = histogram(source_text)
-	print('~-~-~-~-~-~-~-~-~ Histogram ~-~-~-~-~-~-~-~-~')
-	print(histogram)
-	unique = sys.argv[2:]
-	if 'unique_words' in unique:
-		print('\n~-~-~-~-~-~-~-~-~ Unique Words ~-~-~-~-~-~-~-~-~')
-		print(unique_words(histogram))
-		
+    source_text = sys.argv[1]
+    unique_arg = sys.argv[2:]
+    frequency_arg = sys.argv[3:]
+    word_arg = sys.argv[4:]
+    histogram_data = histogram(source_text)
+    print('~-~-~-~-~-~-~-~-~ Histogram ~-~-~-~-~-~-~-~-~')
+    print(histogram_data)
+    if 'unique_words' in unique_arg:
+        print('\n~-~-~-~-~-~-~-~-~ Unique Words ~-~-~-~-~-~-~-~-~')
+        print(unique_words(histogram_data))
+    if 'frequency:' in frequency_arg:
+        print('\n~-~-~-~-~-~-~-~-~ Frequency of Word ~-~-~-~-~-~-~-~-~')
+        print(frequency(word_arg[0], histogram_data))
