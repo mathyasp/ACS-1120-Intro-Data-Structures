19dbaf3adaf777a14b40d8f1482b8565cea834a3
mathyasp <mathyastp@gmail.com>
2024-09-17 15:21:33 -0400

    Added weights to the sampling

--- Code/random_word.py
+++ Code/random_word.py
@@ -9,7 +9,13 @@ def random_word(histogram):
     words = list(histogram.keys())
     rand_num = random_number()
     valid_words = [word for word in words if word.isalpha()]
-    return valid_words[rand_num % len(valid_words)]
+    weights = [histogram[word] for word in valid_words]
+    total = sum(weights)
+    rand_num = rand_num % total
+    for i, weight in enumerate(weights):
+        rand_num -= weight
+        if rand_num <= 0:
+            return valid_words[i]
 
 if __name__ == '__main__':
     source_text = sys.argv[1]
