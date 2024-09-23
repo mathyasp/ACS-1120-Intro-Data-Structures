ecad13f065d58ea43326598835b7728e1308f003
mathyasp <mathyastp@gmail.com>
2024-09-23 19:58:54 -0400

    Implemented Listogram class, fixed test to match data types

--- Code/listogram.py
+++ Code/listogram.py
@@ -20,25 +20,49 @@ class Listogram(list):
 
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
-        # TODO: Increase word frequency by count
+        for list in self:
+            if list[0] == word:
+                list[1] += count
+                break
+        else:
+            new_list = [word, count]
+            self.append(new_list)
+            self.types += 1
+        self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        for list in self:
+            if list[0] == word:
+                return list[1]
+        return 0
 
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for list in self:
+            if list[0] == word:
+                return True
+        return False
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        if self.__contains__(target):
+            for i, list in enumerate(self):
+                if list[0] == target:
+                    return i
+        return None
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
+        words = [list[0] for list in self]
+        weights = [list[1] / self.tokens for list in self]
+        return random.choices(words, weights=weights)[0]
 
 
 def print_histogram(word_list):
