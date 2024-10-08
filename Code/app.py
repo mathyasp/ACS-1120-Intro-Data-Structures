71c187ce1132be1b5b57a83e0c9137fbc5662a06
mathyasp <mathyastp@gmail.com>
2024-10-08 08:25:57 -0400

    Added sentence generation to app.py

--- Code/app.py
+++ Code/app.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from random_word import random_word
 from histogram import histogram
+from markov_chain import MarkovChain
 
 
 app = Flask(__name__)
@@ -9,12 +10,16 @@ app = Flask(__name__)
 # TODO: Initialize your histogram, hash table, or markov chain here.
 # Any code placed here will run only once, when the server starts.
 histogram = histogram('data/metamorphosis.txt')
+word_list = list(histogram.keys())
+markov_chain = MarkovChain(word_list)
 
 @app.route("/")
 def home():
     """Route that returns a web page containing the generated text."""
-    word = random_word(histogram)
-    return f"<p>{word}</p>"
+    # word = random_word(histogram)
+    # return f"<p>{word}</p>"
+    sentence = markov_chain.generate_sentence()
+    return f"<p>{sentence}</p>"
 
 
 if __name__ == "__main__":
